bbcd0e5c07b240826d0b3a1f7bfa36152252727a
L.Sinigaglia <l.sinigaglia@almaviva.it>
2024-06-23 02:56:31 +0200

    added routers to main

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from app.api.oauth2 import router as oauth2_router
 from app.api.upload_transcript_cv import router as upload_transcript_cv_router
 from app.api.coverletter import router as cover_letter_router
+from app.api.get_cover_letter import router as get_cover_letter_router
+from app.api.get_cv import router as get_cv_router
+
 from app.init_db import init_db
 
 
@@ -19,6 +22,9 @@ async def root():
 app.include_router(oauth2_router)
 app.include_router(upload_transcript_cv_router)
 app.include_router(cover_letter_router)
+app.include_router(get_cover_letter_router)
+app.include_router(get_cv_router)
+
 
 if __name__ == "__main__":
     import uvicorn
