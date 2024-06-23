c4ca9f2abfad859a11dd7b1c8332f6b71ea441d0
L.Sinigaglia <l.sinigaglia@almaviva.it>
2024-06-23 02:44:49 +0200

    added get cv and cover letter

--- app/api/upload_transcript_cv.py
+++ app/api/upload_transcript_cv.py
@@ -84,3 +84,13 @@ async def uploadAndTranscript_cv(user_id: int, file: UploadFile = File(...), db:
 
     db_cv = crud.create_cv(db=db, cv=cv_data)
     return db_cv 
+
+
+@router.get("/cv/{user_id}")
+async def get_cv(user_id: int, db: Session = Depends(database.get_db)):
+    # Query the database for the user's CV
+    user_cv = db.query(models.Cv).filter(models.Cv.user_id == user_id).order_by(desc(models.Cv.created_at)).first()
+    if not user_cv:
+        raise HTTPException(status_code=404, detail="CV not found for the user")
+    
+    return {"cv_text": user_cv.text_pdf}
