aec30b350f9e9e0288a781e9a773f77d5a658bd3
L.Sinigaglia <l.sinigaglia@almaviva.it>
2024-06-26 21:21:23 +0200

    added query time

--- app/api/get_cover_letter.py
+++ app/api/get_cover_letter.py
@@ -1,4 +1,5 @@
 
+import time
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -11,14 +12,22 @@ router = APIRouter()
 
 @router.get("/coverletter/{user_id}")
 async def get_cover_letter(user_id: int, db: Session = Depends(database.get_db)):
-    print("#########")
+    start_time = time.time()
+    print("Start processing request")
+
     try:
-        print("#########")
+        query_start_time = time.time()
         user_cover_letter = db.query(models.Coverletter).filter(models.Coverletter.user_id == user_id).order_by(desc(models.Coverletter.created_at)).first()
+        query_end_time = time.time()
+        print(f"Query execution time: {query_end_time - query_start_time:.4f} seconds")
+
         if user_cover_letter is None:
             print(f"No cover letter found for user_id: {user_id}")
-        if not user_cover_letter:
             raise HTTPException(status_code=404, detail="Cover letter not found for the user")
+
+        end_time = time.time()
+        print(f"Total execution time: {end_time - start_time:.4f} seconds")
+
         return {"cover_letter": user_cover_letter.text}
     except SQLAlchemyError as e:
         print(f"SQLAlchemy Error: {e}")
